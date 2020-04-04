7918bf74467921b254ea905c5c74aa0217b17934
fausio luis <fausioluis@live.com>
2020-04-04 21:22:40 +0200

    Explicity creation and Implicity

--- Namespaces/Program.cs
+++ Namespaces/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using Namespaces.Admin;
+using Namespaces.HR;
 
 namespace Namespaces
 {
+    #region Explicity creation 
+    //namespace MyAppClass
+    //{
+    //    namespace Admin
+    //    {
+    //        class Employee
+    //        {
+    //            public void add() { }
+    //            public void update() { }
+    //        }
+    //    }
+
+    //    namespace HR
+    //    {
+    //        class Employee
+    //        {
+    //            public void CalculateSalary() { }
+    //        }
+    //    }
+    //}
+    #endregion
+    #region Implicity
+
+    namespace Admin
+    {
+        class Employee
+        {
+            public void add() { }
+            public void update() { }
+        }
+    }
+    namespace HR
+    {
+        class Employee
+        {
+            public void CalculateSalary() { }
+        }
+    }
+    #endregion
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Admin.Employee employee_Admin = new Admin.Employee();
+            employee_Admin.add();
+            employee_Admin.update();
+
+            HR.Employee employee_HR = new HR.Employee();
+            employee_HR.CalculateSalary();
+
+
+            Console.ReadKey();
         }
     }
 }
